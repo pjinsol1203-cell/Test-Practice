1deffcfa69d06656b566fda603b2aa3056479323
딘도리 <pjinsol1203@gmail.com>
2025-12-04 23:03:05 +0900

    a-1 연습

---
+++ a-1/js/index.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    //slide
+    let count=0
+    const train = document.querySelector(".train")
+
+    setInterval(()=>{
+        count++
+        train.style.transition =`all 0.6s ease 0s`
+        train.style.transform = `translateY(${-25*count}%)`
+
+        if(count==3){
+            setTimeout(()=>{
+                count=0
+                train.style.transition =`none`
+                train.style.transform = `translateY(${-25*count}%)`
+            },800)
+        }
+        },3000)
+
+    //tab menu
+    const tabs = document.querySelectorAll(".tabs a")
+    const tabDesc = document.querySelectorAll(".tab-desc ul")
+
+    tabs.forEach((tab,idx)=>{
+        tab.addEventListener("click",()=>{
+            tabs.forEach(a=>{a.classList.remove("on")})
+            tab.classList.add("on")
+            tabDesc.forEach(ul=>{ul.classList.remove("on")})
+            tabDesc[idx].classList.add("on")
+        })
+    })
+
+    //popup
+    const popup = document.querySelector(".popup")
+    const btnClose = document.querySelector("#btn-close")
+    const btnOpen = document.querySelector("#btn-open")
+
+    btnOpen.addEventListener("click",()=>{
+        popup.classList.add("on")
+    })
+    btnClose.addEventListener("click",()=>{
+        popup.classList.remove("on")
+    })
+    })
